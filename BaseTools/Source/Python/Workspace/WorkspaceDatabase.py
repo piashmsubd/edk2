9c38f32f87da3215b584d83da9ec46b28b2219d3
Shorif Uddin <gooogleabal@gmail.com>
2025-01-04 01:45:18 +0300

    Update WorkspaceDatabase.py

--- BaseTools/Source/Python/Workspace/WorkspaceDatabase.py
+++ BaseTools/Source/Python/Workspace/WorkspaceDatabase.py
@@ -62,8 +62,6 @@ class WorkspaceDatabase(object):
         }
 
         _CACHE_ = {}    # (FilePath, Arch)  : <object>
-        def GetCache(self):
-            return self._CACHE_
 
         # constructor
         def __init__(self, WorkspaceDb):
@@ -80,22 +78,31 @@ class WorkspaceDatabase(object):
 
         # key = (FilePath, Arch=None, Target=None, Toolchain=None)
         def __getitem__(self, Key):
-            FilePath, Arch, Target, Toolchain = Key
-            if isinstance(FilePath, str):
-                # Handle string FilePath appropriately
-                Ext = "UNKNOWN"
+            FilePath = Key[0]
+            KeyLength = len(Key)
+            if KeyLength > 1:
+                Arch = Key[1]
             else:
-                # Ensure FilePath is an expected object type
-                if hasattr(FilePath, 'Type'):
-                    Ext = FilePath.Type
-                else:
-                    Ext = "UNKNOWN"
-            
+                Arch = None
+            if KeyLength > 2:
+                Target = Key[2]
+            else:
+                Target = None
+            if KeyLength > 3:
+                Toolchain = Key[3]
+            else:
+                Toolchain = None
+
+            # if it's generated before, just return the cached one
+            Key = (FilePath, Arch, Target, Toolchain)
+            if Key in self._CACHE_:
+                return self._CACHE_[Key]
+
+            # check file type
             BuildObject = self.CreateBuildObject(FilePath, Arch, Target, Toolchain)
             self._CACHE_[Key] = BuildObject
             return BuildObject
-
-        def CreateBuildObject(self, FilePath, Arch, Target, Toolchain):
+        def CreateBuildObject(self,FilePath, Arch, Target, Toolchain):
             Ext = FilePath.Type
             if Ext not in self._FILE_TYPE_:
                 return None
@@ -105,24 +112,22 @@ class WorkspaceDatabase(object):
 
             # get the parser ready for this file
             MetaFile = self._FILE_PARSER_[FileType](
-                FilePath,
-                FileType,
-                Arch,
-                MetaFileStorage(self.WorkspaceDb, FilePath, FileType)
-            )
-
+                                FilePath,
+                                FileType,
+                                Arch,
+                                MetaFileStorage(self.WorkspaceDb, FilePath, FileType)
+                                )
             # always do post-process, in case of macros change
             MetaFile.DoPostProcess()
             # object the build is based on
             BuildObject = self._GENERATOR_[FileType](
-                FilePath,
-                MetaFile,
-                self,
-                Arch,
-                Target,
-                Toolchain
-            )
-
+                                    FilePath,
+                                    MetaFile,
+                                    self,
+                                    Arch,
+                                    Target,
+                                    Toolchain
+                                    )
             return BuildObject
 
     # placeholder for file format conversion
@@ -130,17 +135,18 @@ class WorkspaceDatabase(object):
         def __init__(self, WorkspaceDb):
             self.WorkspaceDb = WorkspaceDb
 
+        # key = FilePath, Arch
         def __getitem__(self, Key):
             pass
 
-    # Constructor of WorkspaceDatabase
+    ## Constructor of WorkspaceDatabase
     #
-    # @param DbPath           Path of database file
-    # @param GlobalMacros     Global macros used for replacement during file parsing
-    # @param RenewDb=False    Create new database file if it's already there
+    # @param DbPath             Path of database file
+    # @param GlobalMacros       Global macros used for replacement during file parsing
+    # @param RenewDb=False      Create new database file if it's already there
     #
     def __init__(self):
-        self._DB = dict()
+        self.DB = dict()
         # create table for internal uses
         self.TblDataModel = DataClass.MODEL_LIST
         self.TblFile = []
@@ -150,47 +156,57 @@ class WorkspaceDatabase(object):
         self.BuildObject = WorkspaceDatabase.BuildObjectFactory(self)
         self.TransformObject = WorkspaceDatabase.TransformObjectFactory(self)
 
-    # Summarize all packages in the database
+    def SetFileTimeStamp(self,FileId,TimeStamp):
+        self.TblFile[FileId-1][6] = TimeStamp
+
+    def GetFileTimeStamp(self,FileId):
+        return self.TblFile[FileId-1][6]
+
+
+    ## Summarize all packages in the database
     def GetPackageList(self, Platform, Arch, TargetName, ToolChainTag):
         self.Platform = Platform
         PackageList = []
-        Pa = self.BuildObject(self.Platform, Arch, TargetName, ToolChainTag)
-
+        Pa = self.BuildObject[self.Platform, Arch, TargetName, ToolChainTag]
+        #
         # Get Package related to Modules
         #
         for Module in Pa.Modules:
-            ModuleObj = self.BuildObject(Module, Arch, TargetName, ToolChainTag)
+            ModuleObj = self.BuildObject[Module, Arch, TargetName, ToolChainTag]
             for Package in ModuleObj.Packages:
                 if Package not in PackageList:
                     PackageList.append(Package)
-
+        #
         # Get Packages related to Libraries
         #
         for Lib in Pa.LibraryInstances:
-            LibObj = self.BuildObject(Lib, Arch, TargetName, ToolChainTag)
+            LibObj = self.BuildObject[Lib, Arch, TargetName, ToolChainTag]
             for Package in LibObj.Packages:
                 if Package not in PackageList:
                     PackageList.append(Package)
 
-        for Package in Pa.Packages:
-            if Package not in PackageList:
-                PackageList.append(Package)
-
         return PackageList
+
+    ## Summarize all platforms in the database
+    def PlatformList(self):
+        RetVal = []
+        for PlatformFile in [item[3] for item in self.TblFile if item[5] == MODEL_FILE_DSC]:
+            try:
+                RetVal.append(self.BuildObject[PathClass(PlatformFile), TAB_COMMON])
+            except:
+                pass
+        return RetVal
 
     def MapPlatform(self, Dscfile):
         Platform = self.BuildObject[PathClass(Dscfile), TAB_COMMON]
         if Platform is None:
-            EdkLogger.error('build', PARSER_ERROR, "Failed to parse DSC file: %s" % Dscfile)
+            EdkLogger.error('build', PARSER_ERROR, "Failed to parser DSC file: %s" % Dscfile)
         return Platform
-
-BuildDB = WorkspaceDatabase()
 
 ##
 #
 # This acts like the main() function for the script, unless it is 'import'ed into another
 # script.
 #
-##
 if __name__ == '__main__':
     pass
